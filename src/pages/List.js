697b176e4ae816923fed713b885b707664ce7658
bruno <brunocpessoal@gmail.com>
2019-10-11 16:47:49 +0000

    created and implemented the reservation screen of spots and

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, Image, StyleSheet, ScrollView } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-  return <View />
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(tech => tech.trim());
+
+      setTechs(techsArray);
+    })
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => <SpotList key={tech} tech={tech}/>)}
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 40
+  }
+});
